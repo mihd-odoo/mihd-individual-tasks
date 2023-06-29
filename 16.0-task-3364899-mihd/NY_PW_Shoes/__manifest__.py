3ab29b70a25a0f502d41b0234a955170219f50c5
mihd-odoo <mihd@odoo.com>
2023-06-29 11:48:08 -0400

    Task NY P&W Shoes

---
+++ 16.0-task-3364899-mihd/NY_PW_Shoes/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'NY P&W Shoes',
+    'summary': "Module to handle NY P&W Shoes requirements",
+    'description': "Module to add new fields and calculate price automatically",
+    'license':'OPL-1',
+    'author': 'mihd',
+    'website': 'http://localhost:8069',
+    'category': 'NYPWShoes/Product',
+    'depends': ['product','stock'],
+    'data': ["views/product_template_inherit.xml"],
+    'demo': [
+        ],
+    'author': 'MIHD',
+    'application': True,
+}
